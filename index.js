18d76b116759b209b5ebe8647048e88cc0022fc5
Richie Molina <jrmolin90@gmail.com>
2017-10-02 18:55:48 -0400

    Inital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+<script>
+    function myFunction() {
+        // Declare variables
+        var input, filter, ul, li, a, i;
+        input = document.getElementById('myInput');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementByTagName('li');
+
+        // Loop through list, hide those who don't match query
+        for (i=0; i < li.length; i++) {
+            a = li[i].getElementsbyTagName("a")[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+</script>
